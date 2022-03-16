433256c1449dc2c6483612a38755b1800fe46ca6
Tali Kalev <talkalev@gmail.com>
2022-03-16 15:46:41 +0200

    first question done

---
+++ ex1_nogalevy_talikalev.py
@@ -0,0 +1,19 @@
+def question1():
+    counter = 0
+    n1, n2 = input("Please enter 2 numbers: ").split()
+    n1 = [int(x) for x in n1]
+    n2 = [int(x) for x in n2]
+    for x in n1:
+        if x in n2:
+            counter += 1
+
+    print(counter)
+
+question1()
+
+#option = input("Please enter request: ")
+
+#while isinstance(option, int) and int(option) in range(1,5):
+
+
+#    option = input("Please enter request: ")
